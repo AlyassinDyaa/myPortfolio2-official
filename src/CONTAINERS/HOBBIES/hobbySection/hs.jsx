64cebf144e4f2a911d0c7d97a1e52d120d7d1643
Dyaa Alyassin <dyaaalyassin@yahoo.com>
2023-04-30 15:44:03 -0400

    hobbies sections

--- src/CONTAINERS/HOBBIES/hobbySection/hs.jsx
+++ src/CONTAINERS/HOBBIES/hobbySection/hs.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import gsap from 'gsap';
 import './hs.css';
 
@@ -9,46 +9,62 @@ import image4 from "../../../Assets/hobbiesList/dcomics.jpg"
 import image5 from "../../../Assets/hobbiesList/writing.jpg"
 
 const Hs = () => {
-
-  const items = document.querySelectorAll('.item')
+  const [items, setItems] = useState([]);
 
   const expand = (item, i) => {
-  items.forEach((it, ind) => {
-    if (i === ind) return
-    it.clicked = false
-  })
-  gsap.to(items, {
-    width: item.clicked ? '15vw' : '8vw',
-    duration: 2,
-    ease: 'elastic(1, .6)'
-  })
+    items.forEach((it, ind) => {
+      if (i === ind) return;
+      it.clicked = false;
+      it.classList.remove('expanded');
+    });
+    gsap.to(items, {
+      width: item.clicked ? '15vw' : '8vw',
+      duration: 2,
+      ease: 'elastic(1, .6)'
+    });
   
-  item.clicked = !item.clicked
-  gsap.to(item, {
-    width: item.clicked ? '42vw' : '15vw',
-    duration: 2.5,
-    ease: 'elastic(1, .3)'
-  })
-}
+    item.clicked = !item.clicked;
+    item.classList.toggle('expanded');
+    gsap.to(item, {
+      width: item.clicked ? '42vw' : '15vw',
+      duration: 2.5,
+      ease: 'elastic(1, .3)'
+    });
+  };
 
-items.forEach((item, i) => {
-  item.clicked = false
-  item.addEventListener('click', () => expand(item, i))
-})
+  useEffect(() => {
+    const newItems = document.querySelectorAll('.item');
+    setItems(newItems);
+  }, []);
 
+  useEffect(() => {
+    items.forEach((item, i) => {
+      item.clicked = false
+      item.addEventListener('click', () => expand(item, i))
+    });
+  }, [items]);
 
   return (
     <div className="group">
-      <h1>TITLE</h1>
-      <div>
-        <div className="item" style={{ backgroundImage: `url(${image1})` }}></div>
-        <div className="item" style={{ backgroundImage: `url(${image2})` }}></div>
-        <div className="item" style={{ backgroundImage: `url(${image3})` }}></div>
-        <div className="item" style={{ backgroundImage: `url(${image4})`}}></div>
-        <div className="item" style={{ backgroundImage: `url(${image5})` }}></div>
-      </div>
-  </div>
-  )
+            <div className="item" style={{ backgroundImage: `url(${image1})` }}>
+              <h1 className="title">SWIMMING</h1>
+              
+            </div>
+            <div className="item" style={{ backgroundImage: `url(${image2})` }}>
+              <h1 className="title">TAE KWON DO</h1>
+            </div>
+            <div className="item" style={{ backgroundImage: `url(${image3})` }}>
+              <h1 className="title">RUNNING</h1>
+            </div>
+            <div className="item" style={{ backgroundImage: `url(${image4})` }}>
+              <h1 className="title">COMICS, SCI FI</h1>
+            </div>
+            <div className="item" style={{ backgroundImage: `url(${image5})` }}>
+              <h1 className="title">READING</h1>
+            </div>
+    </div>
+  );
 }
 
-export default Hs
+
+export default Hs;
