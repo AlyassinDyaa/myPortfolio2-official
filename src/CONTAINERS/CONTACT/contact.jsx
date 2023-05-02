89600a2c60c6215e6a5402a775e7662943913b18
Dyaa Alyassin <dyaaalyassin@yahoo.com>
2023-05-01 22:37:23 -0400

    delicate solutions and partial contact section done

--- src/CONTAINERS/CONTACT/contact.jsx
+++ src/CONTAINERS/CONTACT/contact.jsx
@@ -9,28 +9,40 @@ const contact = () => {
 
         <section class="get-in-touch">
          <h1 class="title">Get in touch</h1>
-         <form class="contact-form row">
-            <div class="form-field col x-50">
-            <label class="label" for="name">Name</label>
-               <input id="name" class="input-text js-input" type="text" required/>
-            </div>
-            <div class="form-field col x-50">
-            <label class="label" for="email">E-mail</label>
-               <input id="email" class="input-text js-input" type="email" required/>
-            </div>
-            <div class="form-field col x-50">
-            <label class="label" for="name">Subject</label>
-               <input id="name" class="input-text js-input" type="text" required/>
-            </div>
-            <div class="form-field col x-100">
-            <label class="label" for="message">Message</label>
-               <input id="message" class="input-text js-input" type="text" required/>
-            </div>
-            <div class="form-field col x-100 align-center">
-               <button class="btn-head" type="submit" value="Submit">SUBMIT</button>
-            </div>
-         </form>
+
+         <div className='contact-form'>
+               <div className='contact-section'>
+                     <form class="contact-form row">
+                     <div class="form-field col x-50">
+                     <label class="label" for="name">Name</label>
+                        <input id="name" class="input-text js-input" type="text" required/>
+                     </div>
+                     <div class="form-field col x-50">
+                     <label class="label" for="email">E-mail</label>
+                        <input id="email" class="input-text js-input" type="email" required/>
+                     </div>
+                     <div class="form-field col x-50">
+                     <label class="label" for="name">Subject</label>
+                        <input id="name" class="input-text js-input" type="text" required/>
+                     </div>
+                     <div class="form-field col x-100">
+                     <label class="label" for="message">Message</label>
+                        <input id="message" class="input-text js-input" type="text" required/>
+                     </div>
+                     <div class="form-field col x-100 align-center">
+                        <button class="btn-head" type="submit" value="Submit">SUBMIT</button>
+                     </div>
+                  </form>
+               </div>
+
+               <div className='contact-details'>
+                  <h1>hi</h1>
+               </div>
+      </div>
+
       </section>
+
+  
       
    </div>
   )
